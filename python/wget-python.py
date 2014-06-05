dd274056a6ee49728e5018c3a3b72979f6b454f6
attackdebris <attackdebris@wirelessdefence.org>
2014-06-05 18:24:55 +0100

    python wget client

--- python/wget-python.py
+++ python/wget-python.py
@@ -1,1 +1,25 @@
-import urllib; urllib.urlretrieve('https://github.com/attackdebris/scriptFTF/raw/master/scriptFTF.gz', 'scriptFTF.gz')
+#!/usr/bin/ruby
+#
+# wget-python.py version 0.1
+#
+# babel-sf ( https://github.com/attackdebris/babel-sf )
+#
+
+import sys
+import urllib
+
+instructions = "wget-python.py - ( https://github.com/attackdebris/babel-sf )\n" +\
+"\nUsage:" +\
+"\npython wget-python.py [http(s) server resource] [local filename]" +\
+"\ne.g. python wget-python.py https://github.com/attackdebris/babel-sf/archive/master.zip master.zip"
+
+if len(sys.argv) !=3 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+  print instructions
+  sys.exit()
+elif len(sys.argv) ==3:
+  HTTP_RESOURCE=sys.argv[1]
+  LOCAL_FILE=sys.argv[2]
+  print "Starting wget-python.py ( https://github.com/attackdebris/babel-sf )\n"
+  print "Attempting to download the following resource: " + HTTP_RESOURCE
+  urllib.urlretrieve(HTTP_RESOURCE, LOCAL_FILE)
+  print "Complete, resource saved as: " + LOCAL_FILE
