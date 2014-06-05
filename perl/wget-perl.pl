26132ea146f82b54bf5699d98d9a77a8555135ad
attackdebris <attackdebris@wirelessdefence.org>
2014-06-05 18:03:27 +0100

    perl wget client

--- perl/wget-perl.pl
+++ perl/wget-perl.pl
@@ -8,14 +8,14 @@ $LOCAL_FILE=$ARGV[1];
 if ($num_args == 0) {
 	print"wget-perl.pl - ( https://github.com/attackdebris/babel-sf )\n";
 	print"\nUsage:"; 
-        print"\nperl [http(s) server resource] [local filename]";	
+        print"\nperl wget-perl.pl [http(s) server resource] [local filename]";	
         print"\ne.g. perl wget-perl.pl https://github.com/attackdebris/babel-sf/archive/master.zip master.zip\n"
 }
 elsif ($num_args != 2) {
 	print"2 arguments are required, please check your syntax.\n";
 }
 elsif ($num_args == 2) {
-	print "Starting wget-powershell.ps1 ( https://github.com/attackdebris/babel-sf )\n\n";
+	print "Starting wget-perl.pl ( https://github.com/attackdebris/babel-sf )\n\n";
 	print "Attempting to download the following resource: $HTTP_RESOURCE\n";
 	mirror("$HTTP_RESOURCE", "$LOCAL_FILE");
 	print "Complete, resource saved as $LOCAL_FILE\n";
