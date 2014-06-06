ed36a57ff2dcd5cd8023f51523925e8db79496b5
attackdebris <attackdebris@wirelessdefence.org>
2014-06-06 19:43:07 +0100

    python http server

---
+++ python/http-python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# http-python.py version 0.1
+#
+# babel-sf ( https://github.com/attackdebris/babel-sf )
+#
+# really simple syntax/not really required by added for completeness
+#
+
+import sys
+import SimpleHTTPServer
+import SocketServer
+
+instructions = 	"msf-meterpreter-bind_tcp.py ( https://github.com/attackdebris/babel-sf )\n" +\
+		"\nUsage:" +\
+		"\npython http-python.py [bind port]" +\
+		"\ne.g. python http-python.py 8080"
+
+if len(sys.argv) !=2 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+  print instructions
+  sys.exit()
+elif len(sys.argv) ==2:
+  PORT_NUMBER=int(sys.argv[1])
+  Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+  httpd = SocketServer.TCPServer(("", PORT_NUMBER), Handler)
+  print "Starting http-python.py ( https://github.com/attackdebris/babel-sf )\n"
+  print ("Listening on port %s..." % (PORT_NUMBER))
+  httpd.serve_forever()
