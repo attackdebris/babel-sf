5617c5a676ea038ba2437848089f1b8cc72079d9
attackdebris <matt@localhost.localdomain>
2020-10-27 17:39:15 +0000

    Migrate legacy python to python2

---
+++ python2/http-python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# http-python.py version 0.2
+#
+# babel-sf ( https://github.com/attackdebris/babel-sf )
+#
+# really simple syntax/not really required by added for completeness
+#
+
+import sys
+import SimpleHTTPServer
+import SocketServer
+
+instructions = 	"http-python.py ( https://github.com/attackdebris/babel-sf )\n" +\
+		"\nUsage:" +\
+		"\n  python http-python.py [bind port]" +\
+		"\n  e.g. python http-python.py 80"
+
+if len(sys.argv) !=2 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+  print instructions
+  sys.exit()
+elif len(sys.argv) ==2:
+  PORT_NUMBER=int(sys.argv[1])
+  Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+  httpd = SocketServer.TCPServer(("", PORT_NUMBER), Handler)
+  print "Starting http-python.py ( https://github.com/attackdebris/babel-sf )\n"
+  print ("Listening on port %s..." % (PORT_NUMBER))
+  httpd.serve_forever()
