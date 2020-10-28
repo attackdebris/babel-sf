8a706aa3ec4cdbab59b2b81c88a9fb99b8938b0e
attackdebris <attackdebris@gmail.com>
2020-10-28 13:45:07 +0000

    Add wget-python

---
+++ python3/wget-python.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# wget-python.py - Python 3 version 0.1
+#
+# babel-sf ( https://github.com/attackdebris/babel-sf )
+#
+
+import sys
+from urllib.request import urlretrieve
+
+instructions = "wget-python.py ( https://github.com/attackdebris/babel-sf )\n" +\
+"\nUsage:" +\
+"\n  python wget-python.py [HTTP(S) server resource] [local filename]" +\
+"\n  e.g. python wget-python.py https://github.com/attackdebris/babel-sf/archive/master.zip master.zip"
+
+if len(sys.argv) !=3:
+  print (instructions)
+  sys.exit()
+elif len(sys.argv) ==3:
+  HTTP_RESOURCE=sys.argv[1]
+  LOCAL_FILE=sys.argv[2]
+  print ("Starting wget-python.py ( https://github.com/attackdebris/babel-sf )\n")
+  print ("Attempting to download the following resource: " + HTTP_RESOURCE)
+  urlretrieve(HTTP_RESOURCE, LOCAL_FILE)
+  print ("Complete, resource saved as: " + LOCAL_FILE)
